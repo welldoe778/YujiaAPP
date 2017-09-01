e44fe83bb8d7a6ec59dcef978520b568a2590b76
haiyun <123>
2017-09-01 16:19:12 +0800

    测试

---
+++ app/src/main/java/com/strong/yujiaapp/MainActivity.java
@@ -0,0 +1,102 @@
+package com.strong.yujiaapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.strong.yujiaapp.fragment.HomeFragment;
+import com.strong.yujiaapp.fragment.MyFragment;
+import com.strong.yujiaapp.fragment.MyFragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private RadioGroup radioGroup;
+    private RadioButton rb_home, rb_my;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        initView();
+    }
+
+    private void initView() {
+        /**
+         * RadioGroup部分
+         */
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        rb_home = (RadioButton) findViewById(R.id.rb_home);
+        rb_my = (RadioButton) findViewById(R.id.rb_my);
+        //RadioGroup选中状态改变监听
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.rb_home:
+                        viewPager.setCurrentItem(0,true);
+                        break;
+                    case R.id.rb_my:
+                        viewPager.setCurrentItem(1, true);
+                        break;
+
+                }
+            }
+        });
+        /**
+         * ViewPager部分
+         */
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        HomeFragment homeFragment = new HomeFragment();
+        MyFragment myFragment = new MyFragment();
+        List<Fragment> alFragment = new ArrayList<Fragment>();
+
+        alFragment.add(homeFragment);
+        alFragment.add(myFragment);
+
+        //ViewPager设置适配器
+        viewPager.setAdapter(new MyFragmentPagerAdapter(getSupportFragmentManager(), alFragment));
+        //ViewPager显示第一个Fragment
+        viewPager.setCurrentItem(0);
+        //ViewPager页面切换监听
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        radioGroup.check(R.id.rb_home);
+                        break;
+                    case 1:
+                        radioGroup.check(R.id.rb_my);
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+
+
+
+
+
+}
